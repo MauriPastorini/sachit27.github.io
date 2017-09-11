ffe7ef77c4a56d7f167be2acc6fb8ab23447ecbe
Sachit Mahajan <sachitmahajan90@gmail.com>
2017-09-11 13:54:41 +0800

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
         var directionsDisplay = new google.maps.DirectionsRenderer;
         var map = new google.maps.Map(document.getElementById('map'), {
           zoom: 6,
-          center: {lat: 41.85, lng: -87.65}
+          center: Taipei,
         });
         directionsDisplay.setMap(map);
 
