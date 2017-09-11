5884061ca1107fd48edfc3fac1820d063927323f
Sachit Mahajan <sachitmahajan90@gmail.com>
2017-09-11 09:38:10 +0800

    Add files via upload

---
+++ airapi.js
@@ -0,0 +1,41 @@
+
+// shttp get request to server 
+function httpGetAsync(https://pm25.lass-net.org/en/, callback)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+    xmlHttp.send(null);
+}
+// getting json data 
+function ProcessRequest() 
+{
+    if ( xmlHttp.readyState == 4 && xmlHttp.status == 200 ) 
+    {
+        if ( xmlHttp.responseText == "Not found" ) 
+        {
+            document.getElementById( "TextBoxCustomerName"    ).value = "Not found";
+            document.getElementById( "TextBoxCustomerAddress" ).value = "";
+        }
+        else
+        {
+            var info = eval ( "(" + xmlHttp.responseText + ")" );
+
+            // No parsing necessary with JSON!        
+            document.getElementById( "TextBoxCustomerName"    ).value = info.jsonData[ 0 ].cmname;
+            document.getElementById( "TextBoxCustomerAddress" ).value = info.jsonData[ 0 ].cmaddr1;
+        }                    
+    }
+}
+
+// posting json data into DOM  
+function UserAction() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("POST", "https://pm25.lass-net.org/en/", false);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.send();
+    var response = JSON.parse(xhttp.responseText);
+}
