8b989df33d94881cc00136c35d5ce80b592f84b1
Sachit Mahajan <sachitmahajan90@gmail.com>
2017-09-14 01:12:17 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,4 @@
   function initMap() {
-        var renderOptions = {
-          draggable : true
-        }; 
         var directionsService = new google.maps.DirectionsService;
         var directionsDisplay = new google.maps.DirectionsRenderer;
         var map = new google.maps.Map(document.getElementById('map'), {
